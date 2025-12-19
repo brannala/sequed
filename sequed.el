65f70a4453cee63ba0495a508705c3e5606df8f5
Bruce Rannala <brannala@ucdavis.edu>
2025-12-19 10:23:40 -0800

    modfied semantic versioning

--- sequed.el
+++ sequed.el
@@ -4,7 +4,7 @@
 
 ;; Author: Bruce Rannala <brannala@ucdavis.edu>
 ;; URL: https://github.com/brannala/sequed
-;; Package-Version: 20251219.100
+;; Package-Version: v2.0.0
 ;; Package-Requires: ((emacs "25.2"))
 ;; License: GNU General Public License Version 3
 
